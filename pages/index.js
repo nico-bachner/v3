37aab99d3a6947ca672fe9f2a1b3fe2edb9dc9c9
Nico Bachner <yo@nicob.dev>
2021-02-06 22:01:45 +0100

    Initial setup

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Nico Bachner</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                
+            </main>
+        </>
+    )
+}
