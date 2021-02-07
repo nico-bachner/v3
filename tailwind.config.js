214ed335e9037f28015f236af7dd0c2f1b590af3
Nico Bachner <yo@nicob.dev>
2021-02-07 20:05:30 +0100

    feat(typography): added Inter web font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
     purge: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: "media",
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Inter", "sans-serif"],
+            },
+        },
     },
     variants: {
         extend: {},
