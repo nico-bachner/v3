c146b081ef07625f90d51e545d2a37a4919ca952
Nico Bachner <yo@nicob.dev>
2021-02-10 12:59:45 +0100

    fix(design): increased z-index of mobile nav

--- components/layout/MobileNav.js
+++ components/layout/MobileNav.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function MobileNav({ pages }) {
     return (
-        <nav className="fixed bottom-0 w-screen bg-white sm:hidden dark:bg-black bg-blur">
+        <nav className="fixed bottom-0 z-50 w-screen bg-white sm:hidden dark:bg-black bg-blur">
             <ul className="grid grid-cols-4 gap-2 px-8 py-2">
                 {pages.map((page, index) => {
                     return (
