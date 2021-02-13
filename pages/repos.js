f0bc2f1fbbcc1ecce2edc8613bcb2573967a1055
Nico Bachner <yo@nicob.dev>
2021-02-13 15:44:24 +0100

    feat: added toggle all switch to repositories list

--- pages/repos.js
+++ pages/repos.js
@@ -5,11 +5,18 @@ import Card from "@components/Card";
 import { useState, useEffect } from "react";
 
 export default function About() {
+    const GitHubApi = "https://api.github.com/users/nico-bachner";
+
+    const [userData, setUserData] = useState(null);
+    const getUserData = () => fetch(GitHubApi).then((res) => res.json());
+    useEffect(() => {
+        getUserData().then((userData) => {
+            setUserData(userData);
+        });
+    }, []);
+
     const [repos, setData] = useState(null);
-    const getData = () =>
-        fetch("https://api.github.com/users/nico-bachner/repos").then((res) =>
-            res.json()
-        );
+    const getData = () => fetch(GitHubApi + "/repos").then((res) => res.json());
     useEffect(() => {
         getData().then((repos) => {
             repos.sort((a, b) => {
@@ -19,13 +26,15 @@ export default function About() {
         });
     }, []);
 
+    const [all, toggleAll] = useState(false);
+
     return (
         <>
             <Title>Repositories | Nico Bachner</Title>
 
             <h1>GitHub Repositories</h1>
             <p className="subtitle">
-                Here are a few of my public GitHub repositories. The rest can be
+                Here are a few of my public GitHub repositories. They can all be
                 found on{" "}
                 <a
                     href="https://github.com/nico-bachner?tab=repositories"
@@ -33,6 +42,8 @@ export default function About() {
                 >
                     GitHub
                 </a>
+                . Currently, I have {userData?.public_repos} public repositories
+                in total.
             </p>
             <section>
                 <h2 className="my-4">Repositories</h2>
@@ -41,7 +52,10 @@ export default function About() {
                         repo.name = repo.name
                             .replaceAll("-", " ")
                             .replaceAll("md", "Markdown");
-                        if (repo.fork != true && repo.stargazers_count > 0) {
+                        if (
+                            repo.fork != true &&
+                            (repo.stargazers_count > 0 || all)
+                        ) {
                             return (
                                 <ExtLink key={index} href={repo.html_url}>
                                     <Card link className="px-6 py-6">
@@ -55,6 +69,18 @@ export default function About() {
                         }
                     })}
                 </div>
+                <button
+                    onClick={() => {
+                        if (all != true) {
+                            toggleAll(true);
+                        } else {
+                            toggleAll(false);
+                        }
+                    }}
+                    className="m-4 link"
+                >
+                    {all == false ? "Show All" : "Show Less"}
+                </button>
             </section>
             <section>
                 <h2 className="my-4">Forks</h2>
