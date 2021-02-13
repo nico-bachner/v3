a4592c793cea369cab43be790fbb5fb4ee993668
Nico Bachner <yo@nicob.dev>
2021-02-13 11:33:59 +0100

    fix(a11y): increased mobile nav tap targets

--- components/layout/MobileNav.js
+++ components/layout/MobileNav.js
@@ -1,33 +1,33 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
+
+import IntLink from "@components/IntLink";
 
 export default function MobileNav({ pages }) {
     const { pathname } = useRouter();
 
     return (
         <nav className="fixed bottom-0 z-50 w-screen bg-white sm:hidden dark:bg-black bg-blur">
-            <ul className="flex p-4 text-sm font-bold justify-evenly">
+            <ul className="flex py-6 text-sm font-bold justify-evenly">
                 {pages.map((page, index) => {
                     return (
                         <li key={index}>
-                            <Link
+                            <IntLink
                                 href={page.href}
                                 as={
                                     page.slug != undefined
                                         ? "/" + page.slug
                                         : "/"
                                 }
+                                className={
+                                    "p-2" +
+                                    " " +
+                                    (pathname == page.href
+                                        ? "text-gray-500"
+                                        : "hover:text-blue-500 dark:hover:text-blue-300")
+                                }
                             >
-                                <a
-                                    className={
-                                        pathname == page.href
-                                            ? "text-gray-500"
-                                            : "hover:text-blue-500 dark:hover:text-blue-300"
-                                    }
-                                >
-                                    {page.title}
-                                </a>
-                            </Link>
+                                {page.title}
+                            </IntLink>
                         </li>
                     );
                 })}
