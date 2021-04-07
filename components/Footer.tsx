79b8dcc9f436ff9a89e87440944631f60fe53d05
Nico Bachner <yo@nicob.dev>
2021-04-07 23:23:01 +0200

    fix(design): improved footer style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,59 +12,39 @@ export default function Footer(props: Props) {
     return (
         <footer className="max-w-2xl mx-auto my-12 mb-24">
             <hr />
-            <nav className="grid grid-cols-2 my-16 gap-y-12 gap-x-6 sm:grid-cols-4">
-                <div className="grid grid-flow-col grid-rows-4 gap-4">
-                    {props.pages.slice(0, 4).map((page, index) => {
+            <nav className="grid gap-12 my-16 sm:grid-cols-2">
+                <div className="grid grid-flow-col grid-cols-2 grid-rows-4 gap-4">
+                    {props.pages.map((page, index) => {
                         return (
-                            <p key={index} className="text-left">
-                                <Link
-                                    href={page.href}
-                                    className="text-gray-light hover:text-gray-strong"
-                                >
-                                    {page.title}
-                                </Link>
+                            <p
+                                key={index}
+                                className={
+                                    'transition duration-300 ease-in-out transform text-gray-light hover:text-gray-strong hover:-translate-y-0.5' +
+                                    ' ' +
+                                    (index < 4
+                                        ? 'text-left'
+                                        : 'text-right sm:text-center')
+                                }
+                            >
+                                <Link href={page.href}>{page.title}</Link>
                             </p>
                         );
                     })}
                 </div>
-                <div className="grid grid-flow-col grid-rows-4 gap-4">
-                    {props.pages.slice(4, 8).map((page, index) => {
+                <div className="grid grid-flow-col grid-cols-2 grid-rows-4 gap-4">
+                    {props.links.map((link, index) => {
                         return (
-                            <p key={index} className="text-right sm:text-left">
-                                <Link
-                                    href={page.href}
-                                    className="text-gray-light hover:text-gray-strong"
-                                >
-                                    {page.title}
-                                </Link>
-                            </p>
-                        );
-                    })}
-                </div>
-                <div className="grid grid-flow-col grid-rows-4 gap-4">
-                    {props.links.slice(4, 8).map((link, index) => {
-                        return (
-                            <p key={index} className="text-left sm:text-right">
-                                <Link
-                                    href={link.href}
-                                    className="text-gray-light hover:text-gray-strong"
-                                >
-                                    {link.title}
-                                </Link>
-                            </p>
-                        );
-                    })}
-                </div>
-                <div className="grid grid-flow-col grid-rows-4 gap-4">
-                    {props.links.slice(0, 4).map((link, index) => {
-                        return (
-                            <p key={index} className="text-right">
-                                <Link
-                                    href={link.href}
-                                    className="text-gray-light hover:text-gray-strong"
-                                >
-                                    {link.title}
-                                </Link>
+                            <p
+                                key={index}
+                                className={
+                                    'transition duration-300 ease-in-out transform text-gray-light hover:text-gray-strong hover:-translate-y-0.5' +
+                                    ' ' +
+                                    (index < 4
+                                        ? 'text-left sm:text-center'
+                                        : 'text-right')
+                                }
+                            >
+                                <Link href={link.href}>{link.title}</Link>
                             </p>
                         );
                     })}
