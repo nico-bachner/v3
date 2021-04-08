4929f9bc464ce8a57e228893dfe3091a8ba4bc65
Nico Bachner <yo@nicob.dev>
2021-04-08 21:40:16 +0200

    fix: fix for failing deployment

--- hooks/supabase.tsx
+++ hooks/supabase.tsx
@@ -1,9 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-    'https://gcuycdssaicfinxufddo.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNjI2NzkxMSwiZXhwIjoxOTMxODQzOTExfQ.bUY4QiCcs9D9lkUMsuspehX_5msT-AB4YnrKOsSbmdg'
-);
+import { supabase } from '../lib/supabase';
 
 export async function useSupabase(database: string) {
     const { data } = await supabase.from(database).select();
