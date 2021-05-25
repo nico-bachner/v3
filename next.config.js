94adade4139089d80387fae3c2ec28987345c07a
Nico Bachner <yo@nicob.dev>
2021-05-25 15:55:09 +0200

    feat(i18n): added domain-based routing

--- next.config.js
+++ next.config.js
@@ -2,5 +2,15 @@ module.exports = {
     i18n: {
         locales: ['en', 'da', 'fr', 'de', 'lb'],
         defaultLocale: 'en',
+        domains: [
+            {
+                domain: 'nicobachner.com',
+                defaultLocale: 'en',
+            },
+            {
+                domain: 'nico.ist',
+                defaultLocale: 'de',
+            },
+        ],
     },
 };
