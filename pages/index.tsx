04f85be8bd634c3b3e78186a67a53508c41df770
Nico Bachner <yo@nicob.dev>
2021-05-25 16:28:44 +0200

    fix: removed unnecessary filter function for articles

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
     const props: HomeProps = {
         projects: projects.filter((project) => project.featured),
-        articles: articles.filter((article) => article.featured),
+        articles,
     };
 
     return { props };
