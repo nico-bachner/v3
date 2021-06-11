1946c2341572e5bee7c27942a6281b9e46883abc
Nico Bachner <yo@nicob.dev>
2021-06-11 20:50:33 +0200

    feat: added animation to logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,7 @@
 import SVG, { Path, Circle } from './SVG';
 
+import styles from './Logo.module.css';
+
 const Logo: React.VFC = () => (
     <SVG width={60} height={60}>
         <title>Nico Logo</title>
@@ -13,7 +15,8 @@ const Logo: React.VFC = () => (
                 'A 10 10, 0, 1, 0, 20 30',
                 'A 5 5, 0, 0, 1, 15 35',
             ]}
-            stroke={3.5}
+            strokeWidth={3.5}
+            className={styles.majorEdge}
         />
         <Path
             commands={[
@@ -22,11 +25,27 @@ const Logo: React.VFC = () => (
                 'A 10 10, 0, 1, 0, 35 15',
                 'A 5 5, 0, 0, 1, 30 20',
             ]}
-            stroke={3.5}
+            strokeWidth={3.5}
+            className={styles.minorEdge}
         />
-        <Circle centre={{ x: 45, y: 15 }} radius={3.5} stroke={3.5} />
-        <Circle centre={{ x: 30, y: 30 }} radius={3.5} stroke={3.5} />
-        <Circle centre={{ x: 15, y: 45 }} radius={3.5} stroke={3.5} />
+        <Circle
+            centre={{ x: 45, y: 15 }}
+            radius={3.5}
+            strokeWidth={3.5}
+            className={styles.centreCircle}
+        />
+        <Circle
+            centre={{ x: 30, y: 30 }}
+            radius={3.5}
+            strokeWidth={3.5}
+            className={styles.centreCircle}
+        />
+        <Circle
+            centre={{ x: 15, y: 45 }}
+            radius={3.5}
+            strokeWidth={3.5}
+            className={styles.centreCircle}
+        />
     </SVG>
 );
 
