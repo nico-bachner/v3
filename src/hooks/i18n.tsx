f7d012705c755db1b04f90c89820c5e65fecf24c
Nico Bachner <yo@nicob.dev>
2021-07-17 20:14:52 +0200

    refactor: create a src/ directory

--- src/hooks/i18n.tsx
+++ src/hooks/i18n.tsx
@@ -1,10 +1,10 @@
 import { useRouter } from 'next/router';
 
-import { en } from 'content/i18n/en';
-import { da } from 'content/i18n/da';
-import { fr } from 'content/i18n/fr';
-import { de } from 'content/i18n/de';
-import { lb } from 'content/i18n/lb';
+import { en } from 'i18n/en';
+import { da } from 'i18n/da';
+import { fr } from 'i18n/fr';
+import { de } from 'i18n/de';
+import { lb } from 'i18n/lb';
 
 export const translations = { en, da, fr, de, lb };
 
