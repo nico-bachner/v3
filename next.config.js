380d8a8b1790c41b432c247798b0b6a350d70a17
Nico Bachner <yo@nicob.dev>
2021-07-19 12:33:28 +0200

    fix: remove design redirect

--- next.config.js
+++ next.config.js
@@ -3,12 +3,4 @@ module.exports = {
         locales: ['en', 'da', 'fr', 'de', 'lb'],
         defaultLocale: 'en',
     },
-    async rewrites() {
-        return [
-            {
-                source: '/design/:path*',
-                destination: 'https://nico-design.vercel.app/design/:path*',
-            },
-        ];
-    },
 };
