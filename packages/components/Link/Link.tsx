bb38be2ab75e04e4ff319a6237bfe60bee7cc509
Nico Bachner <yo@nicob.dev>
2021-07-19 15:16:17 +0200

    feat(components): added transitions to Link component

--- packages/components/Link/Link.tsx
+++ packages/components/Link/Link.tsx
@@ -6,23 +6,25 @@ import type { LinkProps as NextLinkProps } from 'next/link';
 
 type LinkProps = NextLinkProps & {
     variant?: 'highlight' | 'primary' | 'secondary' | 'disabled';
+    className?: string;
 };
 
 const Link: React.FC<LinkProps> = ({
     children,
     variant = 'default',
+    className,
     ...link
 }) => (
     <NextLink {...link}>
         {link.href.toString().startsWith('') ? (
-            <a className={[styles.link, styles[variant]].join(' ')}>
+            <a className={[styles.link, styles[variant], className].join(' ')}>
                 {children}
             </a>
         ) : (
             <a
                 target="_blank"
                 rel="noopener noreferrer"
-                className={[styles.link, styles[variant]].join(' ')}
+                className={[styles.link, styles[variant], className].join(' ')}
             >
                 {children}
             </a>
