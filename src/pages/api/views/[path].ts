74d1f6f24f0d2a2f7404381428f91b5eac9461c0
Nico Bachner <yo@nicob.dev>
2021-07-22 11:39:19 +0200

    fix(api): hopefully fix 404 in production

--- src/pages/api/views/[path].ts
+++ src/pages/api/views/[path].ts
@@ -3,9 +3,15 @@ import { getViews, getUpdatedViews } from '$lib/utils/views';
 import { NextApiHandler } from 'next';
 
 const Views: NextApiHandler = async (req, res) => {
-    const { path: pathString } = req.query;
+    const { path } = req.query;
 
-    const path = decodeURIComponent(pathString as string);
+    if (!path) {
+        throw new Error(`'path' is required`);
+    }
+
+    if (Array.isArray(path)) {
+        throw new Error(`'path' must be a string`);
+    }
 
     if (req.method == 'POST') {
         return res.status(200).json({
