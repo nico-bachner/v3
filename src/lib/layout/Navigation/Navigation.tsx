106c60e67f19064afaf3d5184658d91533bf3dc7
Nico Bachner <yo@nicob.dev>
2021-07-28 14:29:12 +0200

    fix(ux): automatically close menu on route change

--- src/lib/layout/Navigation/Navigation.tsx
+++ src/lib/layout/Navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 import styles from './Navigation.module.css';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useI18n } from '$lib/hooks/useI18n';
 
@@ -9,9 +9,15 @@ import { Logo } from '@nico-bachner/icons-react';
 import Menu from '$lib/components/Menu';
 
 const Navigation: React.VFC = () => {
-    const { pathname } = useRouter();
+    const { pathname, events } = useRouter();
     const { pages } = useI18n();
     const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        events.on('routeChangeStart', () => {
+            setOpen(false);
+        });
+    }, [events]);
 
     return (
         <nav className={styles.nav}>
