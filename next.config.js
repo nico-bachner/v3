9f5f0c52bafe635b1113e49132da4c4b87b24025
Nico Bachner <yo@nicob.dev>
2021-09-09 23:37:42 +0200

    chore: minor housekeeping

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
 module.exports = {
+    reactStrictMode: true,
     i18n: {
         locales: ['en-AU', 'da-DK', 'fr-LU', 'de-LU', 'lb-LU'],
         defaultLocale: 'en-AU',
