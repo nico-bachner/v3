ead3fb56e57ca80c6dacd4a0aae3c82ef659850e
Nico Bachner <yo@nicob.dev>
2021-09-26 16:14:31 +0200

    fix(design): improve NavBar design

--- src/lib/components/NavBar/NavBar.tsx
+++ src/lib/components/NavBar/NavBar.tsx
@@ -12,40 +12,44 @@ const NavBar: React.VFC = () => {
 
     return (
         <nav className={styles.nav}>
-            <Link href="/" className={styles.logo}>
-                <Logo />
-            </Link>
-            <div className={styles.links}>
-                {pages.main.map(({ href, title }) => (
-                    <Text key={href}>
-                        <Link
-                            href={href}
-                            variant={pathname != href ? 'primary' : 'disabled'}
-                            className={styles.link}
-                        >
-                            {title}
-                        </Link>
-                    </Text>
-                ))}
-            </div>
-            <Select.Root
-                value={locale}
-                onChange={({ target }) => {
-                    push({ pathname, query }, pathname, {
-                        locale: target.value,
-                    });
-                }}
-            >
-                {locales?.map((locale) => {
-                    const [languageCode, countryCode] = locale.split('-');
+            <div className={styles.container}>
+                <Link href="/" className={styles.logo}>
+                    <Logo />
+                </Link>
+                <div className={styles.links}>
+                    {pages.main.map(({ href, title }) => (
+                        <Text key={href}>
+                            <Link
+                                href={href}
+                                variant={
+                                    pathname != href ? 'primary' : 'disabled'
+                                }
+                                className={styles.link}
+                            >
+                                {title}
+                            </Link>
+                        </Text>
+                    ))}
+                </div>
+                <Select.Root
+                    value={locale}
+                    onChange={({ target }) => {
+                        push({ pathname, query }, pathname, {
+                            locale: target.value,
+                        });
+                    }}
+                >
+                    {locales?.map((locale) => {
+                        const [languageCode, countryCode] = locale.split('-');
 
-                    return (
-                        <Select.Option key={locale} value={locale}>
-                            {languageCode?.toUpperCase()}
-                        </Select.Option>
-                    );
-                })}
-            </Select.Root>
+                        return (
+                            <Select.Option key={locale} value={locale}>
+                                {languageCode?.toUpperCase()}
+                            </Select.Option>
+                        );
+                    })}
+                </Select.Root>
+            </div>
         </nav>
     );
 };
