3a80a1c9522f61faf1d0b53204a1a63948c51279
Nico Bachner <yo@nicob.dev>
2021-11-04 13:51:54 +0100

    fix: remove papers

--- src/lib/components/InfoCard/index.ts
+++ src/lib/components/InfoCard/index.ts
@@ -1,4 +1,3 @@
 export { default } from './InfoCard'
 export { default as ProjectCard } from './ProjectCard'
 export { default as ArticleCard } from './ArticleCard'
-export { default as PaperCard } from './PaperCard'
